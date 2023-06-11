cad60dd67b16d9afac9b7d57dc6842c2364b4319
Wanderingidea <42114791+Wanderingidea@users.noreply.github.com>
2023-06-11 18:49:36 +0200

    Update mswsacc.pas

--- mswsacc.pas
+++ mswsacc.pas
@@ -1,5 +1,28 @@
 program mswsacc;
 
+(*
+ * mswsacc - Middle Square Weyl Sequence pseudo random number generator accelerator
+ * Generates Gigabytes of random numbers in seconds.
+ * Example: mswsacc < /dev/ttyUSB0 > test.bin
+ *
+ * Cor van Wandelen 6-2023
+ *
+ * License:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *)
+
+
 {$MODE DELPHI}
 {$RANGECHECKS ON}
 {$OPTIMIZATION ON}
